68b3f9927e7e6dc9e5c03ef299bfced4324d37a3
Stijn Jansen <49640326+StijnJansen2000@users.noreply.github.com>
2020-03-09 17:31:18 +0100

    Add files via upload

--- inc/home.inc.php
+++ inc/home.inc.php
@@ -5,7 +5,7 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Config</h5>
-                    <p class="card-text">Go here to set the config for using the library &nbsp;</p>
+                    <p class="card-text">Go here to set the config</p>
                     <a href="?page=config" class="btn btn-primary col-6">Set Config</a>
                 </div>
             </div>
@@ -14,9 +14,9 @@
         <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Get Data</h5>
-                    <p class="card-text">Go here to receive data by using the library &nbsp;</p>
-                    <a href="?page=get" class="btn btn-primary col-6">Get Data</a>
+                    <h5 class="card-title">Data</h5>
+                    <p class="card-text">...</p>
+                    <a href="?page=get" class="btn btn-primary col-6">Data</a>
                 </div>
             </div>
         </div>
@@ -24,9 +24,9 @@
         <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Create</h5>
-                    <p class="card-text">Go here to create a new Sensor or Component with the library</p>
-                    <a href="?page=create" class="btn btn-primary col-6">Create</a>
+                    <h5 class="card-title">Create Gateway</h5>
+                    <p class="card-text">...</p>
+                    <a href="?page=create" class="btn btn-primary col-6">Create Gateway</a>
                 </div>
             </div>
         </div>
@@ -39,7 +39,7 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Edit</h5>
-                    <p class="card-text">Go here to edit a Sensor or Component with the library</p>
+                    <p class="card-text">...</p>
                     <a href="?page=edit" class="btn btn-primary col-6">Edit</a>
                 </div>
             </div>
@@ -49,7 +49,7 @@
             <div class="card">
                 <div class="card-body">
                     <h5 class="card-title">Delete</h5>
-                    <p class="card-text">Go here to delete a Sensor or Component with the library</p>
+                    <p class="card-text">...</p>
                     <a href="?page=delete" class="btn btn-primary col-6">Delete</a>
                 </div>
             </div>
@@ -58,23 +58,9 @@
         <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
             <div class="card">
                 <div class="card-body">
-                    <h5 class="card-title">Write</h5>
-                    <p class="card-text">Go here to write data on a Sensor with the library</p>
-                    <a href="?page=write" class="btn btn-primary col-6">Write Data</a>
-                </div>
-            </div>
-        </div>
-
-    </div>
-
-    <div class="row">
-
-        <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
-            <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Catalog</h5>
-                    <p class="card-text">Go here to get everything from Sentilo with the library</p>
-                    <a href="?page=catalog" class="btn btn-primary col-6">Get Catalog</a>
+                    <h5 class="card-title">Map</h5>
+                    <p class="card-text">...</p>
+                    <a href="?page=write" class="btn btn-primary col-6">Map</a>
                 </div>
             </div>
         </div>
