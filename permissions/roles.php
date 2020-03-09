d17f4ceb905f1dcd4952bbcb3e9d95cc3105b1fb
Stijn Jansen <49640326+StijnJansen2000@users.noreply.github.com>
2020-03-09 17:35:02 +0100

    Add files via upload

--- permissions/roles.php
+++ permissions/roles.php
@@ -3,6 +3,11 @@
 $menuItems = [
     "guest" => [
         array('home', 'Home', 'L'),
-        array('config', 'Config', 'L')
+        array('config', 'Config', 'L'),
+        array('data', 'Data', 'L'),
+        array('create', 'Create', 'L'),
+        array('edit', 'Edit', 'L'),
+        array('delete', 'Delete', 'L'),
+        array('map', 'Map', 'L'),
     ]
 ];
