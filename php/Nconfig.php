d648f1ae14432a3398ed63a4957c0ac9af1a4e35
Stijn Jansen <49640326+StijnJansen2000@users.noreply.github.com>
2020-03-11 14:35:40 +0100

    Add files via upload

---
+++ php/Nconfig.php
@@ -0,0 +1,51 @@
+<?php
+session_start();
+include "dbh.php";
+
+if (isset($_POST['submit'])){
+
+    $name = htmlspecialchars($_POST['name']);
+    $host = htmlspecialchars($_POST['host']);
+    $provider_id = htmlspecialchars($_POST['provider_id']);
+    $token = htmlspecialchars($_POST['token']);
+
+    $query = $conn->prepare('SELECT * FROM `config` WHERE name=:name');
+    $query->execute(array(
+        ':name' => $name
+    ));
+    $result = $query->fetch(PDO::FETCH_ASSOC);
+
+    if ($query->rowCount() > 0) {
+
+        $_SESSION['config'] = 'Config is set';
+        $_SESSION['name'] = $result['name'];
+        $_SESSION['host'] = $result['host'];
+        $_SESSION['provider_id'] = $result['provider_id'];
+        $_SESSION['token'] = $result['token'];
+
+        header("Location: ../?page=config");
+
+    } elseif($query->rowCount() <= 0){
+
+        $query1 = $conn->prepare('INSERT INTO config SET name=:name, host=:host, provider=:id, token=:token');
+        $query1->execute(array(
+            ':name' => $name,
+            ':host' => $host,
+            ':id' => $provider_id,
+            ':token' => $token
+        ));
+
+        $_SESSION['config'] = 'Config is set';
+        $_SESSION['name'] = $result['name'];
+        $_SESSION['host'] = $result['host'];
+        $_SESSION['provider_id'] = $result['provider_id'];
+        $_SESSION['token'] = $result['token'];
+
+        header("Location: ../?page=config");
+
+    } else {
+        $_SESSION['config'] = "Config is incorrect";
+        header("Location: ../?page=config");
+    }
+
+}
