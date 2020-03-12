ba4124c6d2d5566cd78dc8ed5e0899ec7436cb08
Stijn Jansen <49640326+StijnJansen2000@users.noreply.github.com>
2020-03-12 12:16:55 +0100

    Update gateway.inc.php

--- inc/gateway.inc.php
+++ inc/gateway.inc.php
@@ -82,9 +82,6 @@
         </pre>
         <?php
         } else {
-            echo "<pre>";
-            print_r($_SESSION['config']);
-            echo "</pre>";
             echo '<div class="alert alert-primary" role="alert">';
             echo "Please set the config first";
             echo '</div>';
