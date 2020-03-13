18550e5e6c9bc569fb35bf39facd2681d1f08567
Marnick Scholten <m.f.scholten@st.hanze.nl>
2020-03-13 19:10:01 +0100

    convert function

--- php/create.php
+++ php/create.php
@@ -8,6 +8,32 @@ $long = htmlspecialchars($_POST['longitude']);
 $desc = htmlspecialchars($_POST['description']);
 $config_id = htmlspecialchars($_POST['config_id']);
 
+
+function convertToBCD($val){
+    if (strpos($val, '.') !== false) {
+        $vars = explode(".",$val);
+    } elseif (strpos($val, ',')!== false){
+        $vars = explode(".",$val);
+    }
+
+    $deg = $vars[0];
+    $tempma = "0.".$vars[1];
+
+    $tempma = $tempma * 3600;
+    $min = floor($tempma / 60);
+    $sec = $tempma - ($min*60);
+
+    $bcd = $deg . "°" . $min . "," . abs($sec);
+    echo $bcd;
+    return $bcd;
+}
+
+//        echo "<br>";
+//        convertToBCD($long);
+//        echo "<br>";
+//        convertToBCD($lat);
+
+
 $query = $conn->prepare('SELECT * FROM `gateway` WHERE name=:name');
 $query->execute(array(
     ':name' => $name
