494e8b14d1a53b1d9781f158309b50ba7b23b47d
Marnick Scholten <m.f.scholten@st.hanze.nl>
2020-03-17 13:28:49 +0100

    Map, show only gateway with data

--- inc/map.inc.php
+++ inc/map.inc.php
@@ -1,4 +1,4 @@
-<!--<div class="d-flex justify-content-end">-->
+<div class="d-flex justify-content-end">
 <div id="wrapper">
     <!-- Page Content  -->
     <div id="mapContainer">
@@ -12,26 +12,22 @@
             include 'php/dbh.php';
             $i = 0;
             foreach ( $conn->query('SELECT * FROM gateway') as $row ) {
-                ?>
-                <h2><?php echo $row['name']?></h2>
-                <div class="control-group">
-                    <input type="checkbox" class="form-control-input" id="InputAny" name="any">
-                    <label class="form-check-label" for="InputPublicAccess">Data 1</label>
-                </div>
-                <div class="control-group">
-                    <input type="checkbox" class="form-control-input" id="InputAny" name="any">
-                    <label class="form-check-label" for="InputPublicAccess">Data 2</label>
-                </div>
-                <div class="control-group">
-                    <input type="checkbox" class="form-control-input" id="InputAny" name="any">
-                    <label class="form-check-label" for="InputPublicAccess">Data 3</label>
-                </div>
-                <?php
+                $test = $conn->query('SELECT dataname FROM data WHERE gateway_id=' . $row['gateway_id']);
+                if ($test->rowCount() != 0){
+                    echo "<h2>" . $row['name'] . "</h2>";
+                }
+                foreach ($test as $data){
+            ?>
+            <div class="control-group">
+                <input type="checkbox" class="form-control-input" id="InputAny" name="any">
+                <label class="form-check-label" for="InputPublicAccess"><?php echo $data['dataname']?></label>
+            </div>
+            <?php
+                }
             }
             ?>
-
-
             <button type="submit" class="btn btn-primary">Load</button>
         </form>
     </div>
 </div>
+
