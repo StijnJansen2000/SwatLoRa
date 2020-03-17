e3a9add03b13586c35e0284df79480918892917f
Stijn Jansen <49640326+StijnJansen2000@users.noreply.github.com>
2020-03-17 12:22:30 +0100

    Update home.inc.php

--- inc/home.inc.php
+++ inc/home.inc.php
@@ -54,10 +54,7 @@
                 </div>
             </div>
         </div>
-
-    </div>
-
-    <div class="row">
+        
         <div class="col-lg-4 col-md-4 col-sm-12 mb-3">
             <div class="card">
                 <div class="card-body">
@@ -67,6 +64,7 @@
                 </div>
             </div>
         </div>
+
     </div>
 
 </div>
