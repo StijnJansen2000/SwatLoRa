f60e83f47316eff840f3bb25fd19a923e693fcb2
Marnick Scholten <m.f.scholten@st.hanze.nl>
2020-05-13 11:12:05 +0200

    update

--- inc/showData.inc.php
+++ inc/showData.inc.php
@@ -130,20 +130,50 @@
                 </div>
             </form>
             <a href="?page=data" class="btn btn-primary">Go Back</a>
-            <input type="button" onclick="tableToExcel('table2excel', 'Data from: <?= $query['dataName'] ?>')" value="Export to Excel" class="btn btn-primary">
+            <input type="button" id="button-excel"  value="Export to Excel" class="btn btn-primary">
+<!--            onclick="tableToExcel('table2excel', 'Data from: --><?//= $query['dataName'] ?><!--')"-->
 
             <script type="text/javascript">
+                import TableToExcel from "@linways/table-to-excel";
+
+                let button = document.querySelector("#button-excel");
+
+                button.addEventListener("click", e => {
+                    console.log("Clicked");
+                    let table = document.querySelector("#table2excel");
+                    TableToExcel.convert(table);
+                });
+                // var tableToExcel = (function() {
+                //     var uri = 'data:application/vnd.ms-excel;base64,'
+                //         , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head>[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]<meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
+                //         , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
+                //         , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
+                //     return function(table, name) {
+                //         if (!table.nodeType) table = document.getElementById(table)
+                //         var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
+                //         window.location.href = uri + base64(format(template, ctx))
+                //     }
+                // })()
+
+
                 var tableToExcel = (function() {
                     var uri = 'data:application/vnd.ms-excel;base64,'
-                        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head>[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]<meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
+                        , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body><table>{table}</table></body></html>'
                         , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
                         , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
-                    return function(table, name) {
+                    return function(table, name, fileName) {
                         if (!table.nodeType) table = document.getElementById(table)
                         var ctx = {worksheet: name || 'Worksheet', table: table.innerHTML}
-                        window.location.href = uri + base64(format(template, ctx))
+
+                        var link = document.createElement("A");
+                        link.href = uri + base64(format(template, ctx));
+                        link.download = fileName || 'Workbook.csv';
+                        link.target = '_blank';
+                        document.body.appendChild(link);
+                        link.click();
+                        document.body.removeChild(link);
                     }
-                })()
+                })();
             </script>
 
             <?php
